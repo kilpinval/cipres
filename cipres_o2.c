55e9bbad563a9ddffd7358e49182508b6c3c2e12
kilpinval <kilpinval@gmail.com>
2024-12-25 08:37:17 -0900

    Update cipres_o2.c

--- cipres_o2.c
+++ cipres_o2.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
 
+struct personas{
+    char nombre[80];
+    char apellido[80];
+    int edad;
+    
+};
+
+int main(){
+
+    struct personas DatosPersonas[4];
+    printf("Por favor introduce cuatro nombres de tu familia\n");
+    int i;
+    for(i=0; i<4; i++){
+        printf("Personas nro. %d\n",(i+1));
+        printf("Nombre:  \n");
+        gets(DatosPersonas[i].nombre);
+        printf("Apellido: \n");
+        gets(DatosPersonas[i].apellido);
+        printf("Edad: \n");
+        scanf("%d%c", &DatosPersonas[i].edad);
+        printf("*********************");
+    }
+    
+    printf("Las personas de tu familia son: \n");
+    //int i;
+    for(i=0; i<4; i++){
+
+    
+        printf("Persona nro. %d\n",(i+1));
+        printf("Nombre:  \n",DatosPersonas[i].nombre);
+        printf("Apellidos: \n",DatosPersonas[i].apellido);
+        printf("Edad: \n", DatosPersonas[i].edad);
+        printf("********************");
+    }
+    return 0;
+}
